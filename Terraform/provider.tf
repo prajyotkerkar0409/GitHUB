66a82a89cd2dfc3be589e2eecae2669c067984d7
prajyot.kerkar <prajyot.kerkar@accenture.com>
2026-06-26 18:54:38 +0530

    Code setup

---
+++ Terraform/provider.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = var.aws_region
+}
---
+++ Terraform/variable.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment tag"
+  type        = string
+  default     = "dev"
+}
